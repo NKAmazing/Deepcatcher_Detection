b0db66bb4b758d7fa430aa0a9f1f0d4e2127c91e
Nicolas Mayoral <n.mayoral@alumno.um.edu.ar>
2024-05-31 21:33:41 -0300

    Close #4 Prediction History done

--- app/Welcome.py
+++ app/Welcome.py
@@ -1,108 +1,141 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 import requests
+from pages.Authenticate import show_login, show_authenticated_content
 
 st.set_page_config(page_title="Deepcatcher Demo", page_icon=":globe_with_meridians:")
 
-st.title("Deepcatcher Demo")
+# st.title("Deepcatcher Demo")
 
-st.write("Bienvenido a Deepcatcher, una aplicación de detección de imágenes fakes.")
+# st.write("Welcome to Deepcatcher, an image detection app for fake images.")
 
-st.sidebar.title("Menú")
+st.sidebar.title("Deepcatcher Demo")
 
-menu = st.sidebar.radio("Selecciona una opción", ["Inicio", "Registro", "Login"])
+options = st.sidebar.radio("Select an option: ", ["Home", "Prediction", "Register", "Login"])
+
+# Initialize the session state
+if 'authenticated' not in st.session_state:
+    st.session_state.authenticated = False
 
 if 'token' not in st.session_state:
     st.session_state.token = None
 
-def get_user_id(token):
-    url_api = "http://127.0.0.1:8000/user-service/user/id/"
-    headers = {'Authorization': f'Token {token}'}
-    response = requests.get(url_api, headers=headers)
-    if response.status_code == 200:
-        return response.json().get('user_id')
-    else:
-        st.error("Error al obtener el ID del usuario")
-        return None
+# if 'token' not in st.session_state:
+#     st.session_state.token = None
+
+# def get_user_id(token):
+#     url_api = "http://127.0.0.1:8000/user-service/user/id/"
+#     headers = {'Authorization': f'Token {token}'}
+#     response = requests.get(url_api, headers=headers)
+#     if response.status_code == 200:
+#         return response.json().get('user_id')
+#     else:
+#         st.error("Error al obtener el ID del usuario")
+#         return None
 
 
-if menu == "Inicio":
-    st.write("Aquí va el contenido de la página de inicio")
-    # Configurar el menú de navegación
-    selected = option_menu(
-        menu_title=None,  # required
-        options=["Inicio", "Historial"],  # required
-        icons=["house", "person-add"],  # optional
-        menu_icon="cast",  # optional
-        default_index=0,  # optional
-        orientation="horizontal",
-    )
+# if menu == "Inicio":
+#     st.write("Aquí va el contenido de la página de inicio")
+#     # Configurar el menú de navegación
+#     selected = option_menu(
+#         menu_title=None,  # required
+#         options=["Inicio", "Historial"],  # required
+#         icons=["house", "person-add"],  # optional
+#         menu_icon="cast",  # optional
+#         default_index=0,  # optional
+#         orientation="horizontal",
+#     )
 
-    # Actualizar la página según la selección en el menú
-    if selected == "Inicio":
-        st.session_state.page = "inicio"
-    elif selected == "Historial":
-        st.session_state.page = "historial"
+#     # Actualizar la página según la selección en el menú
+#     if selected == "Inicio":
+#         st.session_state.page = "inicio"
+#     elif selected == "Historial":
+#         st.session_state.page = "historial"
 
-    # Configurar las rutas basadas en el estado de la página
-    if st.session_state.page == "inicio":
-        st.title("Página de Inicio")
-        st.write("Bienvenido a la página de inicio.")
-        st.write(st.session_state.token)
+#     # Configurar las rutas basadas en el estado de la página
+#     if st.session_state.page == "inicio":
+#         st.title("Página de Inicio")
+#         st.write("Bienvenido a la página de inicio.")
+#         st.write(st.session_state.token)
         
-        # Get the user_id through a button
-        if st.button("Obtener ID de Usuario"):
-            user_id = get_user_id(st.session_state.token)
-            st.write(f"ID de Usuario: {user_id}")
+#         # Get the user_id through a button
+#         if st.button("Obtener ID de Usuario"):
+#             user_id = get_user_id(st.session_state.token)
+#             st.write(f"ID de Usuario: {user_id}")
 
-    elif st.session_state.page == "historial":
-        st.title("Historial de Uso")
-        st.write("Aquí se mostrará el historial de uso de la aplicación.")
-elif menu == "Registro":
-    st.title("Formulario de Registro")
+#     elif st.session_state.page == "historial":
+#         st.title("Historial de Uso")
+#         st.write("Aquí se mostrará el historial de uso de la aplicación.")
+# elif menu == "Registro":
+#     st.title("Formulario de Registro")
 
-    username = st.text_input("Nombre de Usuario")
-    email = st.text_input("Email")
-    password = st.text_input("Contraseña", type="password")
+#     username = st.text_input("Nombre de Usuario")
+#     email = st.text_input("Email")
+#     password = st.text_input("Contraseña", type="password")
 
-    if st.button("Registrarse"):
-        if not username or not email or not password:
-            st.error("Todos los campos son obligatorios")
-        else:
-            response = requests.post("http://127.0.0.1:8000/user-service/users/", data={
-                'username': username,
-                'password': password,
-                'email': email   
-            })
+#     if st.button("Registrarse"):
+#         if not username or not email or not password:
+#             st.error("Todos los campos son obligatorios")
+#         else:
+#             response = requests.post("http://127.0.0.1:8000/user-service/users/", data={
+#                 'username': username,
+#                 'password': password,
+#                 'email': email   
+#             })
             
-            if response.status_code == 201:
-                st.success("Usuario registrado con éxito")
-            else:
-                try:
-                    error_message = response.json().get('error')
-                except ValueError:  # incluye JSONDecodeError
-                    error_message = "Error inesperado: respuesta no válida del servidor"
-                st.error(error_message)
-else:
-    st.title("Formulario de Login")
+#             if response.status_code == 201:
+#                 st.success("Usuario registrado con éxito")
+#             else:
+#                 try:
+#                     error_message = response.json().get('error')
+#                 except ValueError:  # incluye JSONDecodeError
+#                     error_message = "Error inesperado: respuesta no válida del servidor"
+#                 st.error(error_message)
+# else:
+#     st.title("Formulario de Login")
 
-    username = st.text_input("Nombre de Usuario")
-    password = st.text_input("Contraseña", type="password")
+#     username = st.text_input("Nombre de Usuario")
+#     password = st.text_input("Contraseña", type="password")
 
-    if st.button("Iniciar Sesión"):
-        if not username or not password:
-            st.error("Todos los campos son obligatorios")
-        else:
-            response = requests.post("http://localhost:8000/api/login/", data={
-                'username': username,
-                'password': password
-            })
+#     if st.button("Iniciar Sesión"):
+#         if not username or not password:
+#             st.error("Todos los campos son obligatorios")
+#         else:
+#             response = requests.post("http://localhost:8000/api/login/", data={
+#                 'username': username,
+#                 'password': password
+#             })
             
-            if response.status_code == 200:
-                st.success("Inicio de sesión exitoso")
-            else:
-                try:
-                    error_message = response.json().get('error')
-                except ValueError:  # incluye JSONDecodeError
-                    error_message = "Error inesperado: respuesta no válida del servidor"
-                st.error(error_message)
+#             if response.status_code == 200:
+#                 st.success("Inicio de sesión exitoso")
+#             else:
+#                 try:
+#                     error_message = response.json().get('error')
+#                 except ValueError:  # incluye JSONDecodeError
+#                     error_message = "Error inesperado: respuesta no válida del servidor"
+#                 st.error(error_message)
+
+def home_view():
+    st.title("Welcome to Deepcatcher Demo")
+    st.write("Deepcatcher is a deep learning model that can classify images as real or fake.")
+    st.write("You can use this demo to classify images and view the prediction history.")
+    st.write("Please select an option from the sidebar to get started.")
+
+
+def main():
+    if options == "Home":
+        home_view()
+    elif options == "Prediction":
+        pass
+    elif options == "Register":
+        pass
+    elif options == "Login":
+        # show_login()
+        # # Show authenticated content if the user is authenticated
+        # show_authenticated_content()
+        pass
+
+    
+
+if __name__ == "__main__":
+    main()
--- app/pages/Predict.py
+++ app/pages/Predict.py
@@ -1,14 +1,20 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
 import tensorflow as tf
 import numpy as np
 from PIL import Image
 import requests
+from streamlit_elements import elements, mui
 
 # Model paths
 model_paths = ['../detection_model/models/model_0.h5', '../detection_model/models/model_1.h5', '../detection_model/models/model_2.h5', '../detection_model/models/model_3.h5']
 
 # Load the model
 model = tf.keras.models.load_model(model_paths[3])
+
+st.sidebar.title("Deepcatcher Demo")
+
+options = st.sidebar.radio("Select an option: ", ["Home"])
 
 # Function to preprocess the uploaded image
 def preprocess_image(image, target_size):
@@ -28,74 +34,139 @@ def predict(image_data, model):
     try:
         classes = ['Fake', 'Real']
         prediction = model.predict(image_data)
-        print(prediction)
         pred_values = tf.squeeze(prediction).numpy()
-        print(pred_values)
         prediction = classes[tf.argmax(pred_values)]
-        print(prediction)
-        confidence = pred_values[tf.argmax(pred_values)]*100
+        confidence = pred_values[tf.argmax(pred_values)] * 100
         return prediction, confidence
     except Exception as e:
         st.error(f"Prediction Error: {str(e)}")
         return None, None
-    
+
 def get_user_id(token):
-    url_api = "http://127.0.0.1:8000/user/id/"
+    url_api = "http://127.0.0.1:8000/user-service/user/id/"
     headers = {'Authorization': f'Token {token}'}
     response = requests.get(url_api, headers=headers)
     if response.status_code == 200:
         return response.json().get('user_id')
     else:
-        st.error("Error al obtener el ID del usuario")
+        st.error("Error at getting the User ID.")
         return None
 
-def save_prediction(user_id, predicted_class, confidence, token):
-    # Save the prediction using the Django API
-    url_api = "http://127.0.0.1:8000/predictions/"
+def save_prediction(user_id, predicted_class, confidence, image_file, token):
+    url_api = "http://127.0.0.1:8000/user-service/predictions/"
     headers = {'Authorization': f'Token {token}'}
+    files = {'image': image_file}
     data = {
         'predicted_class': predicted_class,
         'confidence': confidence,
         'user': user_id,
     }
-    response = requests.post(url_api, headers=headers, json=data)
+    response = requests.post(url_api, headers=headers, data=data, files=files)
     if response.status_code == 201:
-        st.success("Resultado de predicción guardado correctamente en la API.")
+        st.success("Prediction Result successfully saved in the API.")
     else:
-        st.error(f"Error al guardar resultado de predicción: {response.status_code}")
+        st.error(f"Error at saving the prediction result: {response.status_code} - {response.json()}")
 
-# Main Streamlit app
-def main():
+def get_prediction_history(token):
+    url_api = "http://127.0.0.1:8000/user-service/predictions/"
+    headers = {'Authorization': f'Token {token}'}
+    response = requests.get(url_api, headers=headers)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        st.error(f"Error at getting predictions history: {response.status_code} - {response.json()}")
+        return []
+
+def display_prediction_history(predictions):
+    if predictions:
+        st.subheader('Predictions History')
+        with elements("history"):
+            for prediction in predictions:
+                with mui.Card(sx={"maxWidth": 700, "margin": "20px auto", "border-radius": 10}):
+                    with mui.Grid(container=True, spacing=2):
+                        with mui.Grid(item=True, xs=12, sm=3):
+                            mui.CardMedia(
+                                component="img",
+                                height="auto",
+                                width="100%",
+                                image=prediction['image'],  
+                                alt="Image Prediction"
+                            )
+                        with mui.Grid(item=True, xs=12, sm=8):
+                            mui.CardContent(sx={"height": "100%"})(
+                                mui.Typography(f"Predicted Class: {prediction['predicted_class']}"),
+                                mui.Typography(f"Confidence: {prediction['confidence']:.2f}%"),
+                                mui.Typography(f"Date and Time: {prediction['timestamp']}")
+                            )
+                    mui.CardActions()(
+                        mui.Button("Details", size="small"),
+                    )
+    else:
+        st.info('No hay predicciones disponibles.')
+
+def predict_view():
     st.title('Image Classification: Real vs. Fake')
+    # Set the File Uploader
     uploaded_files = st.file_uploader("Choose images...", type=["jpg", "jpeg", "png"], accept_multiple_files=True)
 
     if uploaded_files:
-        num_images = len(uploaded_files)
-        cols = st.columns(num_images)  # Create columns for each image
+        # Set the number of columns per row
+        cols_per_row = 3
+        # Calculate the number of rows based on the number of uploaded files
+        rows = (len(uploaded_files) + cols_per_row - 1) // cols_per_row
+        # Iterate over the rows and columns to display the uploaded images
+        for row in range(rows):
+            cols = st.columns(cols_per_row)
+            for col_index in range(cols_per_row):
+                index = row * cols_per_row + col_index
+                if index < len(uploaded_files):
+                    uploaded_file = uploaded_files[index]
+                    with cols[col_index]:
+                        # Preprocess the uploaded image
+                        image = preprocess_image(uploaded_file, target_size=(96, 96))
 
-        for i, uploaded_file in enumerate(uploaded_files):
-            # Preprocess the uploaded image
-            image = preprocess_image(uploaded_file, target_size=(96, 96))
+                        if image is not None:
+                            # Perform prediction
+                            predicted_class, confidence = predict(image, model)
 
-            if image is not None:
-                # Perform prediction
-                predicted_class, confidence = predict(image, model)
+                            if predicted_class is not None:
+                                # Display prediction result
+                                st.image(uploaded_file, caption=f'Uploaded Image ({predicted_class}, {confidence:.2f}%)', use_column_width=True)
+                                
+                                if st.button(f"Save Prediction {index+1}"):
+                                    # Check if the user is authenticated
+                                    if 'authenticated' in st.session_state and st.session_state.authenticated:
+                                        # Get the user ID
+                                        user_id = get_user_id(st.session_state.token)
+                                        # Save the prediction
+                                        save_prediction(user_id, predicted_class, confidence, uploaded_file, st.session_state.token)
+                                    else:
+                                        st.warning('Please login to save the prediction.')
 
-                if predicted_class is not None:
-                    # Display prediction result
-                    cols[i].image(uploaded_file, caption=f'Uploaded Image ({predicted_class}, {confidence:.2f}%)', use_column_width=True)
+def history_view():
+    if 'authenticated' in st.session_state and st.session_state.authenticated:
+        # Display prediction history
+        predictions = get_prediction_history(st.session_state.token)
+        display_prediction_history(predictions)
+    else:
+        st.warning('History is empty.')
+        st.warning('Please login to view prediction history.')
 
-                    # Check if the user is authenticated
-                    if 'authenticated' in st.session_state and st.session_state.authenticated:
-                        # Get user ID and token
-                        user_id = get_user_id()
+# Main Streamlit app
+def main():
+    selected_option = option_menu(
+        menu_title="Main Menu",
+        options=["Predict", "History"],
+        icons=["camera", "clock"],  # optional
+        menu_icon="cast",  # optional
+        default_index=0,
+        orientation="horizontal", # orientation: horizontal (default) or vertical
+    )
+    if selected_option == "Predict":
+        predict_view()
+    elif selected_option == "History":
+        history_view()
 
-                        print("User ID:", user_id)
-
-                        print("Token:", st.session_state.token)
-
-                        # Save the prediction
-                        save_prediction(user_id, predicted_class, confidence, st.session_state.token)
 
 if __name__ == '__main__':
     main()
